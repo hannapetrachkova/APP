964788cff3ed0fefd386176afdb32a55624ad0a4
Hanna Petrachkova <hanna.petrachkova@leverx.com>
2022-05-24 16:58:49 +0300

    add backend

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Task;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "https://red-plant-04864d603.1.azurestaticapps.net")
+public class HomeController {
+    
+    @GetMapping("/")
+    public Task home() {
+        return new Task(1, "Hello from Azure App Service!");
+    }
+}
