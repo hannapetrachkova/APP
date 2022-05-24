0d42f690bf56b55b42aa656f68242ca273799fa1
Hanna Petrachkova <hanna.petrachkova@leverx.com>
2022-05-24 18:54:17 +0300

    add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="https://proud-pebble-0a0b1d903.1.azurestaticapps.net/", headers: "*", methods: "*")
+@CrossOrigin(origins="https://proud-pebble-0a0b1d903.1.azurestaticapps.net/", headers="*", methods="*")
 
 public class HomeController {
     
