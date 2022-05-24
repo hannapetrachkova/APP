637e76c8d5eb5e20952c4a5c05fd086c9d9de52c
Hanna Petrachkova <hanna.petrachkova@leverx.com>
2022-05-24 18:09:07 +0300

    Add

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins="https://lemon-cliff-0d7572703.1.azurestaticapps.net/")
 public class HomeController {
     
     @GetMapping("/")
     public Task home() {
-        return new Task(1, "Hello from Azure App Service!");
+        return new Task(1, "Hello, world!");
     }
 }
